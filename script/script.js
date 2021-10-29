bb3a67cbc3451b64eacc2ec17f171e001c0485f0
Basile <basile.maille@edu.devinci.fr>
2021-10-29 05:24:46 +0200

    :rocket: Navigation menu deployment

--- script/script.js
+++ script/script.js
@@ -1,24 +1,41 @@
 //MENU BURGER 
+if (window.location.pathname == "/index.html") {
+    
+    document.getElementById('contact-link').addEventListener('click', function () {
+        menu_toggle();
+        document.getElementById('home-link').classList.remove('active');
+        document.getElementById('home-link').parentElement.classList.remove('active');
+        document.getElementById('contact-link').classList.add('active');
+        document.getElementById('contact-link').parentElement.classList.add('active');
+    });
+    if (window.location.hash == "#contact-link"){
+        document.getElementById('contact-link').classList.add('active');
+        document.getElementById('contact-link').parentElement.classList.add('active');
+    }
+    else{
+        document.getElementById('home-link').classList.add('active');
+        document.getElementById('home-link').parentElement.classList.add('active');
+    }
+}
+
 
 var menu_icon = document.getElementById('menu-icon');
 var menu = document.getElementById('menu');
 
-function menu_toggle(){
+function menu_toggle() {
     menu.classList.toggle('active');
 };
 menu_icon.addEventListener('click', menu_toggle);
-
 
 //Slider and Anchor Links
 
 var scroll = document.querySelector('.scroll-container');
 var wrapper = document.querySelector('.wrapper');
 
-function anchorChange(index){
-    [].slice.call(scroll.children).forEach(function (ele, index){
+function anchorChange(index) {
+    [].slice.call(scroll.children).forEach(function (ele, index) {
         ele.classList.remove('active');
     });
-    console.log(scroll.children[index]);
     scroll.children[index].classList.add('active');
 }
 
